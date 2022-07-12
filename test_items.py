29af21694fc8912b677cc6e710a5b3a754163b5c
irina.zykh <irina.zykh@zodiacsystems.com>
2022-07-12 22:55:43 +0300

    test_items.py and conftest.py updates

--- test_items.py
+++ test_items.py
@@ -7,6 +7,5 @@ def test_check_add_to_cart_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/")
     WebDriverWait(browser, 5).until(
         EC.visibility_of_element_located((By.ID, "content_inner")))
-    el_value = browser.find_element(By.XPATH, "//form[@id='add_to_basket_form']/button").get_attribute("value")
-    required_value = "Добавить в корзину"
-    assert required_value == el_value, "Found value {} is not equal to required {}".format(el_value, required_value)
+    buttons = browser.find_elements(By.XPATH, "//form[@id='add_to_basket_form']/button")
+    assert len(buttons) == 1, "Len: {} is not equal to required '1'".format(len(buttons))
